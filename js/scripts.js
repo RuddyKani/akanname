380e0c0b36c6dd59db8dd4767b23c2b5492a8304
Purity Mukami <ruddymukamip@gmail.com>
2020-05-25 15:30:39 +0300

    Calculated for day of week

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,22 @@ function checkAkan() {
   alert("Test complete");
   var checkMale=document.getElementById("male").checked;
   var checkFemale=document.getElementById("female").checked;
-  var checkDay=document.getElementById("birthDay").value;
-  var checkMonth=document.getElementById("birthMonth").value;
-  var checkYear=document.getElementById("birthYear").value;
+  var checkDay=parseInt(document.getElementById("birthDay").value);
+  var checkMonth=parseInt(document.getElementById("birthMonth").value);
+  var checkYear=parseInt(document.getElementById("birthYear").value);
   var akanDay=['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
-
+  var century = Math.floor(checkYear/100);
+  var year = checkYear %100;
+  var day =Math.floor(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(checkMonth+1)/10)) + checkDay ) % 7);
+//(d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
   //alert(document.getElementById('gender').value);
   // if (document.getElementById("male").checked = true){
   //   gender1 ="male";
   // }
 
-  alert("checkMale is " + checkMale +" checkFemale " + checkFemale +" Day " + checkDay);
+  console.log(typeof checkYear);
+  alert(typeof checkYear+" checkMale is " + checkMale +" checkFemale " + checkFemale +" Day " + checkDay
++"Year " + century +"Calculated day " + day);
   //alert(document.getElementsByName('gender').);
   //var gender =document.getElementById('gender').value
 }
