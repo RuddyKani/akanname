5a14b5f602b23d7aa9b93dc71d26f35ce956f50b
Purity Mukami <ruddymukamip@gmail.com>
2020-05-25 16:17:54 +0300

    Added male and female names arrays

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,12 @@ function checkAkan() {
   var checkDay=parseInt(document.getElementById("birthDay").value);
   var checkMonth=parseInt(document.getElementById("birthMonth").value);
   var checkYear=parseInt(document.getElementById("birthYear").value);
-  var akanDay=['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+  var akanDay=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  var maleAkan=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+  var femaleAkan=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
   var century = Math.floor(checkYear/100);
   var year = checkYear %100;
-  var day =Math.floor(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(checkMonth+1)/10)) + checkDay ) % 7);
+  var day =Math.floor((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(checkMonth+1)/10)) + checkDay) % 7);
 //(d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
   //alert(document.getElementById('gender').value);
   // if (document.getElementById("male").checked = true){
