a4a9ef0f935620774998774153d8117b71a6c2fb
Purity Mukami <ruddymukamip@gmail.com>
2020-05-25 14:13:19 +0300

    Get data from form using js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 function checkAkan() {
   alert("Test complete");
+  var checkMale=document.getElementById("male").checked;
+  var checkFemale=document.getElementById("female").checked;
+  var checkDay=document.getElementById("birthDay").value;
+  var checkMonth=document.getElementById("birthMonth").value;
+  var checkYear=document.getElementById("birthYear").value;
+  var akanDay=['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+
+  //alert(document.getElementById('gender').value);
+  // if (document.getElementById("male").checked = true){
+  //   gender1 ="male";
+  // }
+
+  alert("checkMale is " + checkMale +" checkFemale " + checkFemale +" Day " + checkDay);
+  //alert(document.getElementsByName('gender').);
+  //var gender =document.getElementById('gender').value
 }
